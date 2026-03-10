a64db90aa649353ea5bcfc3fd3daa6c2ec928158
SunriseSummer <2667647118@qq.com>
2026-03-10 14:07:28 +0800

    Update language.ts

--- packages/opencode/src/lsp/language.ts
+++ packages/opencode/src/lsp/language.ts
@@ -10,7 +10,6 @@ export const LANGUAGE_EXTENSIONS: Record<string, string> = {
   ".coffee": "coffeescript",
   ".c": "c",
   ".cj": "cangjie",
-  ".cangjie": "cangjie",
   ".cpp": "cpp",
   ".cxx": "cpp",
   ".cc": "cpp",
